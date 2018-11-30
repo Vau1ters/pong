62411e2e77f5f5764a4a1b3d8a32d1b4b45586d1
Skroig0010 <go10.2ka3@ezweb.ne.jp>
2018-11-30 15:45:27 +0900

    update

--- TaskCreate/src/main.cpp
+++ TaskCreate/src/main.cpp
@@ -26,53 +26,6 @@ void WifiServersetup()
     Serial.println("server begin");
     delay(100);
 }
- 
-void WifiServerloop(void *arg) {
-
-for(;;) {
-  delay(10);
-  WiFiClient client = server.available();   // listen for incoming clients
-    if (client) {                             // if you get a client,
-    String currentLine = "";                // make a String to hold incoming data from the client
-      while (client.connected()) {            // loop while the client's connected
-        if (client.available()) {             // if there's bytes to read from the client,
-          char c = client.read();             // read a byte, then
-          if (c == '\n') {                    // if the byte is a newline character
-            // if the current line is blank, you got two newline characters in a row.
-            // that's the end of the client HTTP request, so send a response:
-            if (currentLine.length() == 0) {
-              // HTTP headers always start with a response code (e.g. HTTP/1.1 200 OK)
-              // and a content-type so the client knows what's coming, then a blank line:
-              client.println("HTTP/1.1 200 OK");
-              client.println("Content-type:text/html");
-              client.println();
-              // the content of the HTTP response follows the header:
-              client.print("Click <a href=\"/H\">here</a> to turn the LCD GREEN.<br>");
-              client.print("Click <a href=\"/L\">here</a> to turn the LCD RED.<br>");
-              // The HTTP response ends with another blank line:
-              client.println();
-              // break out of the while loop:
-              break;
-            } else {    // if you got a newline, then clear currentLine:
-              currentLine = "";
-            }
-          } else if (c != '\r') {  // if you got anything else but a carriage return character,
-            currentLine += c;      // add it to the end of the currentLine
-          }
-          // Check to see if the client request was "GET /H" or "GET /L":
-          if (currentLine.endsWith("GET /H")) {
-            M5.Lcd.fillScreen(GREEN);              // GET /H turns the LCD GREEN
-          }
-          if (currentLine.endsWith("GET /L")) {
-            M5.Lcd.fillScreen(RED);                // GET /L turns the LCD RED
-          }
-        }
-      }
-      client.stop();
-    }
-    M5.update();
-  }
-}
 
 #define SerialDebug true  // Set to true to get Serial output for debugging
 
@@ -171,15 +124,6 @@ void MPU9250loop(void *arg)
     } // if (IMU.delt_t > 500)
   }
 }
-
-void Mainloop(void *arg) {
-  for(;;) {
-    static int cnt = 0;
-    Serial.printf("Maintask thread_cnt=%ld\n", cnt++);
-    delay(1200);
-  }
-}
-
 
 void setup() {
   M5.begin();
