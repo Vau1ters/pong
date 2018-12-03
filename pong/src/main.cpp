8658ccc59a56d457e7c6229c014cd826266a501f
Skroig0010 <go10.2ka3@ezweb.ne.jp>
2018-12-03 14:08:10 +0900

    merge TaskCreate and pong; it can communicates the x position of paddle to the other one

--- pong/src/main.cpp
+++ pong/src/main.cpp
@@ -1,4 +1,244 @@
 #include <M5Stack.h>
+#include <WiFi.h>
+#include "utility/MPU9250.h"
+#include "utility/quaternionFilters.h"
+
+//prototype
+void setOtherPaddleState(int);
+
+
+boolean isServer;
+WiFiClient conn;
+
+#if 1
+#define port 1234
+#define ssid "nya"
+#define password "nyannyan"
+#endif
+
+static void startAP() {
+  isServer = true;
+  Serial.println("btnA pressed; starting WiFi AP");
+  WiFi.softAP(ssid, password);
+  Serial.print("local IP address: ");
+  Serial.println(WiFi.softAPIP());
+
+  Serial.println("waiting for client...");
+  isServer = true;
+  WiFiServer server(port);
+  server.begin();
+  while (!(conn = server.available())) {
+    Serial.print(".");
+    delay(100);
+  }
+  server.end();
+  Serial.println("connected");
+}
+
+static void connectAP() {
+  Serial.println("btnB pressed; connecting to WiFi AP");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("connected");
+  Serial.print("local IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.print("gateway IP address: ");
+  Serial.println(WiFi.gatewayIP());
+
+  Serial.println("connecting to server...");
+  isServer = false;
+  // Assuming the gateway is the other device
+  //while (!conn.connect("192.168.43.22", port)) {
+  while (!conn.connect(WiFi.gatewayIP(), port)) {
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println("connected");
+}
+
+static void WiFiSetup() {
+  M5.Lcd.println("press btn A or B");
+  while (true) {
+    if (M5.BtnA.wasPressed()) {
+      startAP();
+      break;
+    }
+    if (M5.BtnB.wasPressed()) {
+      connectAP();
+      break;
+    }
+    delay(100);
+    M5.update();
+  }
+  M5.Lcd.clear();
+  M5.update();
+}
+
+// An event from the other device would be one of:
+//  0. The other device started the game
+//     * (none)
+//  0. The other device hit the ball
+//     * x, dx, dy
+//  1. The other device failed to hit the ball
+//     * (none)
+//  2. The other device moved the paddle
+//     * x
+typedef enum {
+  START,
+  HIT,
+  MISSHIT,
+  MOVE,
+  EVENT_NUM
+} evtype_t;
+
+struct StartEvent { evtype_t type; };
+struct HitEvent { evtype_t type; uint32_t x; float dx, dy; };
+struct MissHitEvent { evtype_t type; };
+struct MoveEvent { evtype_t type; uint32_t x; };
+#define EventMaxSize 16
+
+static void notifyStartGame() {
+  union { uint8_t buf[EventMaxSize]; struct StartEvent e; } buf;
+  memset(&buf, 0, sizeof(buf));
+  buf.e.type = START;
+  conn.write(buf.buf, sizeof(buf.buf));
+}
+
+static void notifyPaddleHit(uint32_t x, float dx, float dy) {
+  union { uint8_t buf[EventMaxSize]; struct HitEvent e; } buf;
+  memset(&buf, 0, sizeof(buf));
+  buf.e.type = HIT;
+  buf.e.x = x;
+  buf.e.dx = dx;
+  buf.e.dy = dy;
+  conn.write(buf.buf, sizeof(buf.buf));
+}
+
+static void notifyPaddleMissHit() {
+  union { uint8_t buf[EventMaxSize]; struct MissHitEvent e; } buf;
+  memset(&buf, 0, sizeof(buf));
+  buf.e.type = MISSHIT;
+  conn.write(buf.buf, sizeof(buf.buf));
+}
+
+static void notifyPaddleMove(uint32_t x) {
+  union { uint8_t buf[EventMaxSize]; struct MoveEvent e; } buf;
+  memset(&buf, 0, sizeof(buf));
+  buf.e.type = MOVE;
+  buf.e.x = x;
+  conn.write(buf.buf, sizeof(buf.buf));
+}
+
+static int readbuf(uint8_t buf[EventMaxSize]) {
+  int ret = 0;
+  for(size_t len = 0; len < EventMaxSize; len += ret) {
+    ret = conn.read(&buf[len], sizeof(uint8_t[EventMaxSize]) - len);
+    if (ret < 0) {
+      //error("read error; aborting\n");
+      return -1;
+    }
+  }
+  return EventMaxSize;
+}
+
+static void WiFiLoop(void *arg) {
+  union {
+    uint8_t buf[EventMaxSize];
+    struct { evtype_t type; } x;
+    struct StartEvent start;
+    struct HitEvent hit;
+    struct MissHitEvent missHit;
+    struct MoveEvent move;
+  } buf;
+  _Static_assert(sizeof(buf) == sizeof(buf.buf), "x");
+
+  while (true) {
+    if(readbuf(buf.buf) == -1) continue;
+    switch (buf.x.type) {
+      case START: // Start game
+        // FIXME: The ball starts moving on the screen (towards the opponent's side)
+        // Reset the ball location to (50%, 50%), ...
+        Serial.println("start");
+        break;
+      case HIT: // Hit
+        // FIXME: Reset the ball location information to (x, 0)
+        // buf.hit.{x,dx,dy}
+        Serial.printf("hit: %d, %f, %f\n", buf.hit.x, buf.hit.dx, buf.hit.dy);
+        break;
+      case MISSHIT: // MissHit
+        // FIXME: Show the "WON" screen
+        Serial.println("misshit");
+        break;
+      case MOVE: // Move
+        // FIXME: opponentPaddlePosition = buf.move.x;
+        Serial.printf("move: %d\n", buf.move.x);
+        setOtherPaddleState(buf.move.x);
+        break;
+      default:
+        Serial.println("unexpected event");
+    }
+  }
+  conn.stop();
+}
+
+
+MPU9250 IMU;
+
+void MPU9250setup() {
+  Wire.begin();
+  IMU.initMPU9250();
+
+  // Read the WHO_AM_I register, this is a good test of communication
+  byte c = IMU.readByte(MPU9250_ADDRESS, WHO_AM_I_MPU9250);
+  Serial.print("MPU9250 "); Serial.print("I AM "); Serial.print(c, HEX);
+  Serial.print(" I should be "); Serial.println(0x71, HEX);
+
+  Serial.println("MPU9250 is online...");
+
+  // Start by performing self test and reporting values
+  IMU.MPU9250SelfTest(IMU.SelfTest);
+  Serial.print("x-axis self test: acceleration trim within : ");
+  Serial.print(IMU.SelfTest[0],1); Serial.println("% of factory value");
+  Serial.print("y-axis self test: acceleration trim within : ");
+  Serial.print(IMU.SelfTest[1],1); Serial.println("% of factory value");
+  Serial.print("z-axis self test: acceleration trim within : ");
+  Serial.print(IMU.SelfTest[2],1); Serial.println("% of factory value");
+  Serial.print("x-axis self test: gyration trim within : ");
+  Serial.print(IMU.SelfTest[3],1); Serial.println("% of factory value");
+  Serial.print("y-axis self test: gyration trim within : ");
+  Serial.print(IMU.SelfTest[4],1); Serial.println("% of factory value");
+  Serial.print("z-axis self test: gyration trim within : ");
+  Serial.print(IMU.SelfTest[5],1); Serial.println("% of factory value");
+
+  // Calibrate gyro and accelerometers, load biases in bias registers
+  IMU.calibrateMPU9250(IMU.gyroBias, IMU.accelBias);
+
+  Serial.println("MPU9250 initialized for active data mode....");
+}
+
+void getSensorValue(void *arg) {
+  if (!(IMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)) return; 
+  IMU.readAccelData(IMU.accelCount);  // Read the x/y/z adc values
+  IMU.getAres();
+
+  // Now we'll calculate the accleration value into actual g's
+  // This depends on scale being set
+  IMU.ax = (float)IMU.accelCount[0]*IMU.aRes; // - accelBias[0];
+  IMU.ay = (float)IMU.accelCount[1]*IMU.aRes; // - accelBias[1];
+  IMU.az = (float)IMU.accelCount[2]*IMU.aRes; // - accelBias[2];
+
+  IMU.readGyroData(IMU.gyroCount);  // Read the x/y/z adc values
+  IMU.getGres();
+
+  // Calculate the gyro value into actual degrees per second
+  // This depends on scale being set
+  IMU.gx = (float)IMU.gyroCount[0]*IMU.gRes;
+  IMU.gy = (float)IMU.gyroCount[1]*IMU.gRes;
+  IMU.gz = (float)IMU.gyroCount[2]*IMU.gRes; 
+}
 
 // macro for drawing picture
 #define get(b, i) !!((b) & (1 << (i))) 
@@ -39,7 +279,7 @@ void drawBall(int x, int y);
 void drawPaddle(int x, int y);
 void flush();
 void gameInit();
-void gameLoop();
+void gameLoop(void *arg);
 void swapBuffer();
 GameObject collideWall(GameObject object);
 bool aabb(GameObject o1, GameObject o2);
@@ -47,11 +287,22 @@ void resetGame();
 
 void setup() {
     M5.begin();
+    Serial.begin(115200);
+
+    MPU9250setup();
+    // Become a WiFi AP or client, and then connect to the other device
+    WiFiSetup();
+
     gameInit();
+
+    xTaskCreate(WiFiLoop, "WiFiLoop", 0x10000, NULL, 1, NULL);
+    TimerHandle_t x = xTimerCreate("Timer", ( 160 / portTICK_PERIOD_MS ), pdTRUE, NULL, gameLoop);
+    xTimerStart( x, 0 );
+
 }
 
 void loop() {
-    gameLoop();
+    //gameLoop();
     M5.update();
 }
 
@@ -109,11 +360,12 @@ void resetGame(){
     ball.vx = ball.vy = 2;
 }
 
-void gameLoop(){
+void gameLoop(void *arg){
     // update
     // move paddle0
     if(M5.BtnC.isPressed())paddle[0].x-=2;
     if(M5.BtnA.isPressed())paddle[0].x+=2;
+    notifyPaddleMove(paddle[0].x);
 
     // move paddle1
     paddle[1].x += paddle[1].vx;
@@ -154,6 +406,14 @@ void gameLoop(){
     M5.Lcd.setCursor(0, 0);
     M5.Lcd.printf("%d, %d", score[0], score[1]);
     swapBuffer();
+
+    M5.update();
+
+    //debug
+    //notifyStartGame();
+    //notifyPaddleHit(1, IMU.ax, IMU.ay);
+    //notifyPaddleMissHit();
+    //notifyPaddleMove(2);
 }
 
 bool aabb(GameObject o1, GameObject o2){
